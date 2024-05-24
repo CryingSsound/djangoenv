3562fc0729987bc8f7c553fceaff49781919ef29
sihyun <idhsh0125@naver.com>
2024-05-24 20:37:32 +0900

    success

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,9 @@
-from django import forms 
-from .models import Post 
+from django import forms
 
-class PostForm(forms.ModelForm): 
-    class Meta: 
-        model = Post 
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
         fields = ('title', 'text',)
